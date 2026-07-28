ddb956422746b9941fbc89a9a3cadde789a082c7
Paulo Sérgio <pamorimmonico@gmail.com>
2026-07-28 13:44:06 -0300

    Remove unused or redundant includes

--- src/btree.c
+++ src/btree.c
@@ -1,11 +1,8 @@
 #include "btree.h"
 #include <errno.h>
 #include <fcntl.h>
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <sys/uio.h>
 #include <unistd.h>
 
